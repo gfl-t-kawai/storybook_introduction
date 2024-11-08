0f2432113dbf3bf12b14111ab1bfa3983c19cd71
t_kawai <t_kawai@global-future-link.co.jp>
2024-11-08 22:55:21 +0900

    Introduction

--- taskbox/.storybook/main.js
+++ taskbox/.storybook/main.js
@@ -1,15 +1,33 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
+
+module.exports = {
+  stories: ['../src/components/**/*.stories.js'],
+  staticDirs: ['../public'],
+
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-interactions',
+    '@storybook/addon-webpack5-compiler-babel',
+    '@chromatic-com/storybook',
+    '@storybook/addon-a11y',
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
+
+  features: {
+    postcss: false,
   },
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
-export default config;
